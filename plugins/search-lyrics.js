bdfea5f8fbe96403adfadc5128e2ee49469ae0a9
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2025-01-10 21:45:01 +0200

    Update search-lyrics.js

--- plugins/search-lyrics.js
+++ plugins/search-lyrics.js
@@ -1,51 +1,21 @@
-/*const axios = require('axios');
-const { cmd } = require('../command');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
 
 cmd({
     pattern: "lyrics",
-    desc: "Get the lyrics of a song.",
-    category: "music",
-    react: "🎶",
+    alias: ["liri","lyric"], 
+    react: "🤍",
+    desc: "ai chat.",
+    category: "main",
     filename: __filename
 },
-async (conn, mek, m, { from, reply, text }) => {
-    // Extract song title and artist from the message text
-    const [songTitle, artist] = text.split('|').map(str => str.trim());
-
-    if (!songTitle || !artist) {
-        return reply("Please provide the song title and artist in the format: `!lyrics <song_title> | <artist>`");
-    }
-
-    try {
-        // Replace spaces with '+' for URL encoding
-        const song = songTitle.replace(/\s+/g, '+');
-        const songArtist = artist.replace(/\s+/g, '+');
-
-        // Construct the API URL
-        const url = `https://api.davidcyriltech.my.id/lyrics?t=song   a={songArtist}`;
-
-        // Fetch lyrics from the API
-        const response = await axios.get(url);
-      const data = response.data;
-
-        // Check if the response contains lyrics
-        if (data.lyrics) {
-            const lyricsMessage = `
-🎶 *Lyrics for*: songTitle by{artist}
-
-${data.lyrics}
-
-*Lyrics provided by MR FRANK OFC*
-            `;
-            return reply(lyricsMessage);
-        } else {
-            return reply("Sorry, I couldn't find the lyrics for this song. Please check the song title and artist.");
-        }
-    } catch (e) {
-        console.error("Error fetching lyrics:", e);
-        reply("¢συℓ∂ ησт ƒєт¢н α lуrιc. ρℓєαѕє тяу αgαιη ℓαтєя.");
-    }
-});
-
-*/
-  
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://api.giftedtech.web.id/api/search/lyrics?apikey=gifted&query=${q}`);
+return reply(data.result);
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
