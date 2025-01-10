ced07ffb47f4d069511d0029b165cbc6d6786b3f
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2025-01-10 23:08:22 +0200

    Update ai-stablediffusion.js

--- plugins/ai-stablediffusion.js
+++ plugins/ai-stablediffusion.js
@@ -1,22 +1,61 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const { fetchJson } = require('../lib/functions')
 
-cmd({
-    pattern: "sd",
-    alias: ["stablediffusion","diffuse"], 
-    react: "♻️",
-    desc: "ai chat.",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let data = await fetchJson(`https://api.davidcyriltech.my.id/diffusion?prompt=${q}`)
-const imageUrl = data.result;
-await conn.sendMessage(m.chat, { image: { url: imageUrl } });
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x24421f=_0x2f4c;(function(_0x1242a2,_0x231919){const _0x4d106b=_0x2f4c,_0x569052=_0x1242a2();while(!![]){try{const _0xeacdf5=-parseInt(_0x4d106b(0xd0))/0x1*(-parseInt(_0x4d106b(0xce))/0x2)+-parseInt(_0x4d106b(0xdf))/0x3+parseInt(_0x4d106b(0xd4))/0x4+-parseInt(_0x4d106b(0xdb))/0x5+-parseInt(_0x4d106b(0xde))/0x6*(parseInt(_0x4d106b(0xd5))/0x7)+parseInt(_0x4d106b(0xd3))/0x8+parseInt(_0x4d106b(0xd9))/0x9;if(_0xeacdf5===_0x231919)break;else _0x569052['push'](_0x569052['shift']());}catch(_0x62e02b){_0x569052['push'](_0x569052['shift']());}}}(_0x2ae3,0xc0535));function hi(){const _0x34996a=_0x2f4c;console[_0x34996a(0xd2)](_0x34996a(0xcf));}function _0x2ae3(){const _0x2bd4ec=['>\x20*Subzero\x20Diffussing\x20Your\x20image...*','800VzaAvv','Hello\x20World!','992nYxMkj','chat','log','8454520bsKyoW','114840nqxKps','27958pgUvMW','result','sendMessage','flux','1711647SlssAB','Please\x20provide\x20a\x20prompt\x20for\x20the\x20image.','1637910UehTMF','Generate\x20an\x20image\x20using\x20AI.','An\x20error\x20occurred:\x20','90BJlDlr','1491753kipEEh','subzeroimg'];_0x2ae3=function(){return _0x2bd4ec;};return _0x2ae3();}function _0x2f4c(_0x58e8db,_0x5323bb){const _0x2ae373=_0x2ae3();return _0x2f4c=function(_0x2f4c31,_0x1b0b5e){_0x2f4c31=_0x2f4c31-0xce;let _0x1b3c80=_0x2ae373[_0x2f4c31];return _0x1b3c80;},_0x2f4c(_0x58e8db,_0x5323bb);}hi();const config=require('../config'),{cmd,commands}=require('../command'),{fetchJson}=require('../lib/functions');cmd({'pattern':_0x24421f(0xd8),'alias':['dalle',_0x24421f(0xe0)],'react':'⚡”„','desc':_0x24421f(0xdc),'category':'main','filename':__filename},async(_0x4a51a3,_0x3da9f9,_0xdd1578,{from:_0x2b3753,quoted:_0x2fdad0,body:_0x1b4ff7,isCmd:_0x13c0d1,command:_0x42de11,args:_0x5352cd,q:_0x4cafd0,isGroup:_0x1a2277,sender:_0x3f7a54,senderNumber:_0x45d0d2,botNumber2:_0x50609b,botNumber:_0x54cea2,pushname:_0x1113fa,isMe:_0x551449,isOwner:_0x330e9c,groupMetadata:_0x2b5391,groupName:_0x412d36,participants:_0x2ce1c1,groupAdmins:_0x5c0633,isBotAdmins:_0xcfc58f,isAdmins:_0x526bcd,reply:_0x370a00})=>{const _0x26bd2b=_0x24421f;try{if(!_0x4cafd0)return _0x370a00(_0x26bd2b(0xda));await _0x370a00(_0x26bd2b(0xe1));let _0x54ef91=await fetchJson('https://api.davidcyriltech.my.id/diffusion?prompt='+_0x4cafd0);const _0x511723=_0x54ef91[_0x26bd2b(0xd6)];await _0x4a51a3[_0x26bd2b(0xd7)](_0xdd1578[_0x26bd2b(0xd1)],{'image':{'url':_0x511723}});}catch(_0xf72188){console['error'](_0xf72188),_0x370a00(_0x26bd2b(0xdd)+_0xf72188['message']);}});
